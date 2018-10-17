0a58eb80f6cbfafcd42806bda2d4a8e77bff6f48
Andi Dittrich <andi@andidittrich.de>
2018-10-17 12:12:14 +0200

    removed /etc/motd symlink

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,6 @@
+### 0.6.0 ###
+Changed: `/etc/motd` is not altered anymore. only `/etc/update-motd.d/11-sysinfo` symlink will be added
+
 ## 0.5.0 ##
 Changed: figlet hostname color to **light blue**
 Removed: Install script "ready" message
--- install.sh
+++ install.sh
@@ -6,19 +6,6 @@ set -e
 # message
 echo "Initializing /etc/update-motd.d structure.."
 
-# move old motd if exists
-if [ -f /etc/motd ]; then
-    mv /etc/motd /etc/motd.old
-fi
-
-# motd is already a symlink ?
-if [ -L /etc/motd ]; then
-    rm /etc/motd
-fi
-
-# create motd symlink
-ln -s /run/motd /etc/motd
-
 # create update-motd.d directory
 if [ ! -d /etc/update-motd.d/ ]; then
     mkdir /etc/update-motd.d
